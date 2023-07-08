a50d17d637e5391b0a5b0dd43fea9a4713236733
José Leonardo <joseleonardosantoos@outlook.com>
2023-07-08 09:46:29 -0300

    completando a classe main

--- src/me/andromedabot/main/AndromedaBot.java
+++ src/me/andromedabot/main/AndromedaBot.java
@@ -1,16 +1,36 @@
 package me.andromedabot.main;
 
+import me.andromedabot.events.MemberJoin;
+import me.andromedabot.commands.Ping;
+import me.andromedabot.events.MemberLeave;
 import me.andromedabot.security.Security;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
+import java.util.EnumSet;
+
 public class AndromedaBot {
 
-    public static void main(String[] args) {
+    public static JDA jda;
+
+    public static void main(String[] args) throws InterruptedException {
         Security security = new Security();
-        JDA jda = JDABuilder.create(security.token,
-                GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_PRESENCES).build();
+        jda = JDABuilder.create(security.token, EnumSet.allOf(GatewayIntent.class)).build();
+
+        // A única coisa que você precisa fazer é instanciar o JDA, chamar awaitReady(), da instância você pode obter todos os canais (MessageChannels, TextChannels, VoiceChannels, seja chamando
+        //get[Text]Channels()
+        //get[Text]ChannelById(id=..)
+        //get[Text]ChannelsByName(name, ignore case))
+        jda.awaitReady();
+
+        //commands
+        jda.addEventListener(new Ping());
+
+        //events
+        jda.addEventListener(new MemberJoin());
+        jda.addEventListener(new MemberLeave());
+
 
     }
 
